9bcc9d92551666dd0eb82a8bccaf375e05f73138
Krzysztof Nasuta <krzysiek.com.pl@wp.pl>
2022-04-14 15:37:07 +0200

    Graph is now working

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Playlist visualizer for Spotify
+Graphical representation of your favourite playlist! Generate a graph, which shows how many songs of an artists you have added (size of a node), and visualizes every songs that
+two artist had recorded together (size of a )
+![Example](./img/kaleidoscope.png)
+## How to use it?
+Input your data in .env file:
+```
+TOKEN=YOUR_TOKEN_FROM_SPOTIFY_API
+PLAYLIST=ID_OF_PLAYLIST
+```
+You can get your token here: [Spotify API](https://developer.spotify.com/console/get-playlist-tracks/). Just click "Get Token". If you want to visualize a private playlist,
+please select adequate scope. Your token should start with "BQ".
+
+You can see yuor playlist ID by clicking Share > Copy Link To PLaylist. Your ID will be visible in the URL. (Example: [5xynfyTPMhpWEVq91TwNQz](https://open.spotify.com/playlist/5xynfyTPMhpWEVq91TwNQz?si=8d6c781f26d1420a))
+
+Required libraries:
+- pyvis
+```
+pip install pyvis
+```
+
+Then just run the python script:
+```
+python main.py
+```
+If the playlist is big, it may take some time, cause not to exceed rate limits every request has a 500 ms cooldown (can be easily changed)
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from unicodedata import name
 from dotenv import load_dotenv
 from pyvis.network import Network
 import os
 import requests
 import json
-import random
+from time import sleep
 
 # API token and playlist ID is stored in .env file (TOKEN and PLAYLIST)
 # You can generate your own token here: https://developer.spotify.com/console/get-playlist-tracks/
@@ -12,9 +11,30 @@ import random
 load_dotenv()
 headers = {'Accept': 'application/json','Content-Type': 'application/json','Authorization': f'Bearer {os.environ["TOKEN"]}'}
 params = {'market': 'PL', 'limit': '100'}
+cooldown = 0.5
 
 artists = {}
 connections = {}
+artist_id = {}
+def get_playlist_name(playlist_id):
+    response = requests.get(f'https://api.spotify.com/v1/playlists/{playlist_id}', headers=headers, params=params)
+    data = json.loads(response.text)
+    sleep(cooldown) # For API rate limits
+    return data["name"]
+
+# Get artist photo, resolutions : 640, 320, 160
+def get_artist_photo(artist_id, resolution=640):
+    response = requests.get(f'https://api.spotify.com/v1/artists/{artist_id}', headers=headers, params=params)
+    data = json.loads(response.text)
+    id = 0
+    match resolution:
+        case 320:
+            id = 1
+        case 160:
+            id = 2
+    sleep(cooldown) # For API rate limits
+    return data["images"][id]['url']
+
 # Get info about artists from Spotify API and then count artists and their connections
 def get_connections(link):
     response = requests.get(link, headers=headers, params=params)
@@ -22,25 +42,57 @@ def get_connections(link):
     for track in data["items"]:
         local_artists = []
         for artist in track["track"]["artists"]:
-            local_artists.append(artist["name"])
+            name = artist["name"]
+            local_artists.append(name)
+            artist_id[name] =  artist["id"]
         for artist in local_artists:
             artists[artist] = artists[artist] + 1 if artist in artists else 1
-            if(artist not in connections):
-                connections[artist] = {}
-        for first_artist in local_artists:
-            for second_artists in local_artists:
-                if(first_artist is not second_artists):
-                    connections[first_artist][second_artists] = connections[first_artist][second_artists] + 1 if second_artists in connections[first_artist] else 1
+        main_artist = local_artists[0]
+        additional_artists = local_artists[1:]
+        if(main_artist not in connections):
+            connections[main_artist] = {}
+        for additional_artist in additional_artists:
+            connections[main_artist][additional_artist] = connections[main_artist][additional_artist] + 1 if additional_artist in connections[main_artist] else 1
     next = data["next"]
-    #if(next != None):
-        #get_connections(next)
+    sleep(cooldown) # For API rate limits
+    if(next != None):
+        get_connections(next)
 get_connections(f'https://api.spotify.com/v1/playlists/{os.environ["PLAYLIST"]}/tracks')
 
 # Visualize a playlists as a network
-net = Network()
+net = Network('1080px', '1920px')
 
+total_songs = 0
+for i in artists:
+    total_songs += artists[i]
 for artist in artists:
     songs = artists[artist]
-    net.add_node(n_id=artist, name=artist, size=songs, color='#'+''.join([random.choice('0123456789ABCDEF') for j in range(6)])) # Random color for every node, looks cool :)
+    if(songs / total_songs >= 0.01 or songs >= 25):
+        net.add_node(n_id=artist, name=artist, size=songs, shape='circularImage', image=get_artist_photo(artist_id=artist_id[artist]))
+    else:
+        net.add_node(n_id=artist, name=artist, size=songs)
+artists_with_edges = {}
+for first_artist in connections:
+    for second_artist in connections[first_artist]:
+        if(first_artist in artists_with_edges):
+            if(second_artist in artists_with_edges[first_artist]):
+                artists_with_edges[first_artist][second_artist] += connections[first_artist][second_artist]
+            else:
+                artists_with_edges[first_artist][second_artist] = connections[first_artist][second_artist]
+        elif(second_artist in artists_with_edges):
+            if(first_artist in artists_with_edges[second_artist]):
+                artists_with_edges[second_artist][first_artist] += connections[first_artist][second_artist]
+            else:
+                artists_with_edges[second_artist][first_artist] = connections[first_artist][second_artist]
+        else:
+            artists_with_edges[first_artist] = {second_artist : connections[first_artist][second_artist]}
+for first_artist in artists_with_edges:
+    for second_artist in artists_with_edges[first_artist]:
+        if(first_artist is second_artist):
+            continue
+        net.add_edge(first_artist, second_artist, value=artists_with_edges[first_artist][second_artist])
 
-net.show(f'Playlist - {os.environ["PLAYLIST"]}.html')
+
+net.show_buttons(filter_=['physics'])
+net.show(f'Playlist - {get_playlist_name(os.environ["PLAYLIST"])}.html')
+
